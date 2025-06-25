ab155b764ca66281dc3b7d5ca5f2deee80268af4
Ashish <bansalgashi083@gmail.com>
2025-06-25 23:08:53 +0530

    changes in searchbar component

--- app/scrap-rates/components/SearchBar.js
+++ app/scrap-rates/components/SearchBar.js
@@ -1,20 +1,34 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useRouter} from "next/navigation";
 
 const SearchBar = () => {
-  return (
-    <div className='flex flex-row  gap-8 '>
-        <div className='w-auto border-2 border-black rounded-2xl grow-1 p-4'>
-            <select >
-                <option>Yamunanagar</option>
-                <option>Jagadhri</option>
-                <option>Chachrauli</option>
-            </select>            
-        </div>
-        <div className='w-auto border-2 border-black rounded-2xl grow-3 p-4'>
-            <input placeholder='search by item name....'></input>
-        </div>
-    </div>
-  )
-}
+  const router = useRouter();
+  
 
-export default SearchBar
+
+
+  const handleCityChange = (e) => {
+    const selectedCity = e.target.value;
+    if (selectedCity) {
+      router.push(`/scrap-rates/${selectedCity}`);
+    }
+  };
+  return (
+    <div className="flex flex-row  gap-8 ">      
+      <div className="w-auto border-2 border-black rounded-2xl grow-1 p-4">
+        <select name="city"  onChange={handleCityChange} >
+          <option value="">--search for cities--</option>
+          <option value="Yamunanagar">Yamunanagar</option>
+          <option value="Jagadhri">Jagadhri</option>
+          <option value="Chachrauli">Chachrauli</option>
+        </select>
+      </div>
+      <div className="w-auto border-2 border-black rounded-2xl grow-3 p-4">
+        <input placeholder="search by item name...."></input>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
